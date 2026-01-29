360454636f7fb5a1808b14dc25f76cd2830209c5
2ufkpfb9daxnik <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2026-01-29 11:22:06 +0900

    1

--- mongosh/14/sample.mongosh.js
+++ mongosh/14/sample.mongosh.js
@@ -1,16 +1,9 @@
-// npm run mql -- mongosh/14/sample.mongosh.js
-
 db.s_users.drop();
 
 db.s_users.insertMany([
-  { name: "Alice", level: 20 },
-  { name: "Bob", level: 22 },
-  { name: "Carol", level: 19 },
+  { name: "Alice", level: 20},
+  { name: "Bob", level: 22},
+  { name: "Carol", level: 15},
 ]);
 
-printjson(
-  db.s_users
-    .find({ level: { $gte: 20 } })
-    .sort({ level: -1 })
-    .toArray(),
-);
+printjson(db.s_users.find({level: {$gte: 20}}).sort({level:-1}).toArray());
